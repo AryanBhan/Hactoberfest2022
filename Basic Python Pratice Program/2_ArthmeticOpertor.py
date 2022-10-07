d243c4371ae34419168946e11419d13fc7f7b936
Aryan Bhan <87280331+AryanBhan@users.noreply.github.com>
2022-10-07 11:03:26 +0530

    Adding Python Code to 2_ArthmeticOpertor.py
    The Above Code will:
    1.Take 2 Input from user of a and b respectively
    2.Then Perform the Arithmetic Operation on them

---
+++ Basic Python Pratice Program/2_ArthmeticOpertor.py
@@ -0,0 +1,14 @@
+# Operator	Name	        Example	
+# +	        Addition	    x + y	
+# -	        Subtraction	    x - y	
+# *	        Multiplication	x * y	
+# /	        Division	    x / y	
+# %	        Modulus	        x % y	
+# **	    Exponentiation	x ** y	
+# //        division	    x // y
+a=int(input("Enter the First Number:"))
+b=int(input("Enter the Second Number:"))
+# Operators
+print("+","-","*","/  ","%","**","//")
+# Operation on a , b
+print(a+b,a-b,a*b,a/b,a%b,a**b,a//b)
